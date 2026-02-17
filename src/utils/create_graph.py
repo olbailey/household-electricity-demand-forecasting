3d415d1bc9b65fa0ded497af18e05a3edae8612f
OliverB-963 <olbailey@outlook.com>
2026-02-17 10:50:42 +0000

    improved code quality of data graphing jupyter notebook

--- src/utils/create_graph.py
+++ src/utils/create_graph.py
@@ -1,11 +1,17 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-def draw_graph(df: pd.DataFrame, data_label: str, title: str, y_label: str, length=20) -> None:
+def draw_graph(df: pd.DataFrame, data_label: str, title: str, y_label: str, x_label, length=20, colour="#1f77b4", other_values=[]) -> None:
     plt.figure(figsize=(length, 5))
-    plt.plot(df["DateTime"], df[data_label], label=data_label)
 
-    plt.xlabel("Time of Day")
+    if other_values is not None:
+        plt.plot(df["Datetime"], df[data_label], label=data_label)
+        for item_label in other_values:
+            plt.plot(df["Datetime"], df[item_label], label=item_label)
+    else:
+        plt.plot(df["Datetime"], df[data_label], label=data_label, color=colour)
+
+    plt.xlabel(x_label)
     plt.ylabel(y_label)
     plt.title(title)
     
